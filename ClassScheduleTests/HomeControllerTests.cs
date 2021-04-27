074547907c66a23813a7b9dcbf81e087ba69adda
Pacifique Ntahurunga <np@npacifique.us>
2021-04-27 00:55:25 -0400

    feat: class schedule

---
+++ ClassScheduleTests/HomeControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using ClassSchedule.Models;
+using ClassSchedule.Controllers;
+
+using System;
+namespace ClassScheduleTests
+{
+    public class HomeControllerTests
+    {
+        public HomeControllerTests()
+        {
+        }
+
+
+        [Fact]
+        public void IndexActionMethod_ReturnsAViewResult()
+        {
+            
+            var unit = new Mock<IClassScheduleUnitOfWork>();
+            var classes = new Mock<IRepository<Class>>();
+            var days = new Mock<IRepository<Day>>();
+            unit.Setup(r => r.Classes).Returns(classes.Object);
+            unit.Setup(r => r.Days).Returns(days.Object);
+
+            var http = new Mock<IHttpContextAccessor>();
+
+            var controller = new HomeController(unit.Object, http.Object);
+
+            var result = controller.Index(0);
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
---
+++ ClassScheduleTests/TeacherControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ClassSchedule.Controllers;
+using ClassSchedule.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace ClassScheduleTests
+{
+    public class TeacherControllerTests
+    {
+        public TeacherControllerTests()
+        {
+        }
+
+        [Fact]
+        public void IndexActionMethod_ReturnsAViewResult()
+        {
+          
+            var rep = new Mock<IRepository<Teacher>>();
+            var controller = new TeacherController(rep.Object);
+
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+
+        }
+    }
+}
